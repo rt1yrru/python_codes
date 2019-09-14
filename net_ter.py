38223a414effdd50137a55ce724359f746da079d
siddht4 <dhar.75.ap@gmail.com>
2019-09-14 11:20:36 +0530

    Create net_ter.py

---
+++ net_ter.py
@@ -0,0 +1,4 @@
+import netifaces
+
+def display():
+  return [netifaces.ifaddresses(iface)[netifaces.AF_INET][0]['addr'] for iface in netifaces.interfaces() if netifaces.AF_INET in netifaces.ifaddresses(iface)]

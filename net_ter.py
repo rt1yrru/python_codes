ff81b0ca7eb7ab00f000d6b2ddd64e41e37bae91
siddht4 <dhar.75.ap@gmail.com>
2019-09-14 11:38:42 +0530

    Update net_ter.py

--- net_ter.py
+++ net_ter.py
@@ -1,4 +1,5 @@
 import netifaces
 
 def display():
-  return [netifaces.ifaddresses(iface)[netifaces.AF_INET][0]['addr'] for iface in netifaces.interfaces() if netifaces.AF_INET in netifaces.ifaddresses(iface)]
+    for _i in netifaces.interfaces():
+	      return (netifaces.ifaddresses(_i))
